01d8a0bd3d59d038c749b4e65947fe7d8a456f1a
NaufalGhifari <85378958+NaufalGhifari@users.noreply.github.com>
2025-07-28 13:25:39 +0700

    Webhook now writes to gsheet

--- webhook.py
+++ webhook.py
@@ -19,14 +19,19 @@ class WebhookServer:
     async def notify(self, request: Request):
         data = await request.json()
         message = data.get("message")
+        gsheet_url = data.get("gsheet_url")
+        gsheet = Gsheet_agent(gsheet_url)
         
         print(f"data: {data}")
         print(f"message:{message}")
 
         row = Gsheet_agent.message_to_row(message, auto=True)
         print(f"row:{row}")
+        gsheet.write_to_sheet(row)
 
-        return {"status": "ok"}
+        return {"status": "ok",
+                "message_received": f"{message}",
+                "row":f"{row}"}
 
     def run(self):
         uvicorn.run(self.app, host=self.host, port=self.port)
